7854ad7e762840c77a985c693b2b460d4ddb1702
Lívia Aristides de Oliveira <118351499+LivAristides@users.noreply.github.com>
2025-08-29 15:01:07 -0300

    Atualiza controllers, repositories e adiciona ProductService

--- src/main/java/com/fatec/itu/product/controllers/ProductController.java
+++ src/main/java/com/fatec/itu/product/controllers/ProductController.java
@@ -4,27 +4,33 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fatec.itu.product.entities.Product;
-import com.fatec.itu.product.repositories.ProductRepository;
+import com.fatec.itu.product.services.ProductService;
 
 @RestController
 @RequestMapping("products")
 public class ProductController {
-    
+    //injeta o repositorio 
     @Autowired
-    private ProductRepository repository;
+    private ProductService service;
     //metodo
     //retorna todos os produtos cadastrados no banco -- repositorio
     @GetMapping
     public List<Product> getProducts(){
 
-    return repository.findAll();
+    return service.getAllProducts();
 
     
    }
+
+   @GetMapping("{id}")
+public Product getProductById(@PathVariable long id) {
+        return service.getProductById(id);
+}
 
 
 }
--- src/main/java/com/fatec/itu/product/repositories/ProductRepository.java
+++ src/main/java/com/fatec/itu/product/repositories/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.fatec.itu.product.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;//faz o selec da tabela 
 
 import com.fatec.itu.product.entities.Product;
 
